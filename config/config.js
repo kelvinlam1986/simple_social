2b7c619601079ad6c4ba61e26c6fa949d72d6621
Minh Lam <kelvincoder@gmail.com>
2019-02-11 17:19:11 +0700

    Initial project

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.NODE_PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || 'kelvinlam1986',
+    mongoUri: process.env.MONGODB_URI ||
+        process.env.MONGO_HOST ||
+        'mongodb://' + (process.env.IP || 'localhost') + ':' +
+        (process.env.MONGO_PORT || '27017') +
+        '/mernproject'
+
+};
+
+export default config;
---
+++ server/express.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import path from 'path';
+import Template from './../template';
+import devBundle from './devBundle';
+import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
+
+const CURRENT_WORKING_DIR = process.cwd()
+const app = express();
+
+devBundle.compile(app);
+
+/* configure express */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
+app.use('/', userRoutes);
+app.use('/', authRoutes);
+
+console.log('template', Template);
+app.get('/', (req, res) => {
+    res.status(200).send(Template());
+});
+
+
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ "error": err.name + ": " + err.message});
+    }
+})
+
+export default app;
