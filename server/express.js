89a6a4dbd0dffd58f2386dbf178a9cba565bf38a
Minh Lam Su <kelvincoder@gmail.com>
2019-06-10 15:26:15 +0700

    Docker multi stage

--- server/express.js
+++ server/express.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compress from 'compression';
-import cors from 'cors';
-import helmet from 'helmet';
-import path from 'path';
-import Template from './../template';
-import devBundle from './devBundle';
-import userRoutes from './routes/user.routes';
-import authRoutes from './routes/auth.routes';
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import cors from "cors";
+import helmet from "helmet";
+import path from "path";
+import Template from "./../template";
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
-const CURRENT_WORKING_DIR = process.cwd()
+const CURRENT_WORKING_DIR = process.cwd();
 const app = express();
 
-devBundle.compile(app);
+if (process.env.NODE_ENV === "development") {
+  const devBundle = require("./devBundle");
+  devBundle.default.compile(app);
+}
 
 /* configure express */
 app.use(bodyParser.json());
@@ -22,21 +24,19 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 
-app.use('/', userRoutes);
-app.use('/', authRoutes);
+app.use("/", userRoutes);
+app.use("/", authRoutes);
 
-console.log('template', Template);
-app.get('/', (req, res) => {
-    res.status(200).send(Template());
+app.get("/", (req, res) => {
+  res.status(200).send(Template());
 });
 
-
 app.use((err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({ "error": err.name + ": " + err.message});
-    }
-})
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  }
+});
 
 export default app;
